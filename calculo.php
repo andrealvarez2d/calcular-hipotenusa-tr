64ea7b3071fdc77050b04ae87b33230c9160ebb0
andrealvarez2d <alvarez.andreabeatriz@gmail.com>
2022-02-08 23:09:24 -0400

    cambio

--- calculo.php
+++ calculo.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular la hipotenusa del triangulo rectangulo</title>
+    <link rel="shortcut icon" href="icono/calculator.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
 <?php
-    echo '<title>Calcular la hipotenusa del triangulo rectangulo</title>';
-    echo '<link rel="shortcut icon" href="icono/calculator.png">';
-    echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
     //declarar variables
     $lado1 = 3;
     $lado2 = 4;
@@ -26,3 +34,5 @@
         echo '</div>';
     }
 ?>
+</body>
+</html>
