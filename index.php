3197161e33c55e78e1d306efe5e688d654009e4c
andrealvarez2d <alvarez.andreabeatriz@gmail.com>
2022-02-08 23:56:04 -0400

    agregar imageno

--- index.php
+++ index.php
@@ -5,17 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calcular la hipotenusa del triangulo rectangulo</title>
-    <link rel="shortcut icon" href="icono/calculator.png">
+    <link rel="shortcut icon" href="img/calculator.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="container col-8 p-5" style="background-color: #fae1dd;">
         <h1 style="color: black;">Calcular la hipotenusa del triangulo rectangulo</h1>
+        <div class="row">
+        <div class="col-4">
         <p>el primer lado es de: 3 cm </p>
         <br/>
         <p>el segundo lado es de: 4 cm </p>
         <br/>
-
+        </div>
+        <div class="col-4" >
+        <img src="img/tr.png" alt="triangulo rectangulo">
+        </div>
+        </div>
         <form method="post" action="calculo.php">
         <input type="submit" name="btn" value="Calcular">
         </form>
